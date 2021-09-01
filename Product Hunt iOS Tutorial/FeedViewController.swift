dc75c0ac5d2623de60a23a453b563eaa29d0106d
Sawyer Cherry <sawyeracherry@gmail.com>
2021-09-01 14:12:31 -0500

    Tests with mock data

--- Product Hunt iOS Tutorial/FeedViewController.swift
+++ Product Hunt iOS Tutorial/FeedViewController.swift
@@ -11,30 +11,42 @@ class FeedViewController: UIViewController {
     
     @IBOutlet weak var feedTableView: UITableView!
     
+    var mockData: [Post] = {
+        var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
+        var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
+        var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
+        return [meTube, boogle, meTunes]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         feedTableView.delegate = self
         feedTableView.dataSource = self
     }
-
-
+    
+    
 }
 
 // MARK: UITableViewDataSource
 extension FeedViewController: UITableViewDataSource {
-   /// Determines how many cells will be shown on the table view.
-   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 3
-   }
-
-   /// Creates and configures each cell.
-   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       return UITableViewCell()
-  }
+    /// Determines how many cells will be shown on the table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mockData.count
+    }
+    
+    /// Creates and configures each cell.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
+        
+        let post =  mockData[indexPath.row]
+        cell.post = post
+        
+        return cell
+    }
 }
 
 // MARK: UITableViewDelegate
 extension FeedViewController: UITableViewDelegate {
-  // Code to handle cell events goes here...
+    // Code to handle cell events goes here...
 }
