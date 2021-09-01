a78694d4b7c4e877f623d915fa4c36e5ab13493d
Sawyer Cherry <sawyeracherry@gmail.com>
2021-09-01 12:54:54 -0500

    created post model

---
+++ Product Hunt iOS Tutorial/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Product Hunt iOS Tutorial
+//
+//  Created by Sawyer Cherry on 9/1/21.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
+
